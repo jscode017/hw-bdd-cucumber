1f0326a31402dd2d54a4d49e79f4b4bc29ec2d85
jscode017 <jasonhack518@gmail.com>
2021-03-02 12:18:26 +0800

    merge hw2

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -7,6 +7,49 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @movies = Movie.all
+    @all_ratings=Movie.all_ratings
+    if !params.include?(:notFirst)
+      session.clear
+    end
+    if !params.include?(:ratings) && !session.include?(:ratings)
+      @movies=Movie.all
+      @ratings_to_show=[]
+      session[:ratings]=Hash.new
+    elsif params.include?(:ratings)
+      @ratings_to_show=params[:ratings].keys
+      if @ratings_to_show.length==0
+        @rating_to_show=[]
+      end
+      @movies=Movie.with_ratings(@ratings_to_show)
+      session[:ratings]=params[:ratings]
+    elsif params.include?(:commit)
+      @movies=Movie.all
+      @ratings_to_show=[]
+      session[:ratings]=Hash.new
+    elsif 
+      @ratings_to_show=session[:ratings].keys
+      if @ratings_to_show.length==0
+        @rating_to_show=[]
+      end
+      @movies=Movie.with_ratings(@ratings_to_show)
+    end
+    @selected_ratings=@ratings_to_show
+    @sort_by=''
+    if params.include?(:sort)
+      @sort_by=params[:sort]
+      session[:sort]=params[:sort]
+    elsif session.include?(:sort)
+      @sort_by=session[:sort]
+    end
+    
+    if @sort_by=='title'
+      @movies=@movies.order('title')
+    elsif @sort_by=='release_date'
+      @movies=@movies.order('release_date')
+    end
+  end
+=begin
     sort = params[:sort] || session[:sort]
     case sort
     when 'title'
@@ -27,7 +70,7 @@ class MoviesController < ApplicationController
       redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
     @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
-  end
+=end
 
   def new
     # default: render 'new' template
@@ -36,7 +79,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    redirect_to movies_path, :notFirst=> true
   end
 
   def edit
@@ -47,14 +90,14 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+    redirect_to movie_path(@movie), :notFirst=> true
   end
 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
+    redirect_to movies_path, :notFirst=> true
   end
   
   private
--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,6 +1,15 @@
 class Movie < ActiveRecord::Base
   
-  def self.all_ratings
-    %w(G PG PG-13 NC-17 R)
-  end
+	def self.all_ratings
+	%w(G PG PG-13 NC-17 R)
+	end
+	def all_ratings=(ratings)
+	      @all_ratings=ratings
+	end
+	def self.with_ratings(ratings_list)
+	  if ratings_list.nil? || ratings_list.length==0
+	       return Movie.all
+	  end
+	  return Movie.where('rating in (?)',ratings_list)   
+	end
 end
